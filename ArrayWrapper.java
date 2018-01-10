904c999e6623ad031fa19a6e6cfdf750f1b7ef44
Győrfi István <gyorfisti@gmail.com>
2018-01-10 09:52:11 +0100

    finished the task in pair-programming

--- ArrayWrapper.java
+++ ArrayWrapper.java
@@ -12,23 +12,55 @@ public class ArrayWrapper {
 
     // Returns the number at 'index'.
     public int access(int index) {
-        return 0;
+        return array[index];
     }
 
     // Returns the index of 'number' if it's in the array, otherwise -1;
     public int search(int number) {
-        return 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == number) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     // Inserts 'number' at 'index' into the array shifting elements if necessary.
     //
     // e.g. the result of inserting 42 at index 3 into [0, 1, 2, 3, 4] is [0, 1, 2, 42, 3, 4]
     public void insert(int index, int number) {
+        int[] newArray = new int[this.array.length + 1];
+        int oldIndex = 0;
+
+        for (int i = 0; i < newArray.length; i++) {
+            if (i == index) {
+                newArray[index] = number;
+            } else {
+                newArray[i] = array[oldIndex];
+                oldIndex++;
+            }
+        }
+        this.array = newArray;
     }
 
     // Deletes the element at 'index' from the array.
     //
     //  e.g. the result of deleting index 2 from [0, 1, 2, 3, 4] is [0, 1, 3, 4]
     public void delete(int index) {
+        int[] newArray = new int[this.array.length - 1];
+        int newIndex = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (i == index) {
+                continue;
+            } else {
+                newArray[newIndex] = array[i];
+                newIndex++;
+                
+            }
+        }
+        this.array = newArray;
+
+
     }
 }
